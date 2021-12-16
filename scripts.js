ca3872c1d72337fa0d0445d20ba9fed8cca479fd
Emir Erten <erte0004@algonquinlive.com>
2021-12-15 23:05:09 -0500

    Completes Assignment

--- scripts.js
+++ scripts.js
@@ -20,22 +20,21 @@ const $dateContainer = document.getElementById('date-container')
 const $weekContainer = document.getElementById('week-container')
 
 
-const $colorOptions = document.getElementById('color-options')
-const $colorWhite = document.getElementById('color-white')
-const $colorGreen = document.getElementById('color-green')
-const $colorYellow = document.getElementById('color-yellow')
+const $secondsOptions = document.getElementById('seconds-options')
 
-const $turnOn = document.getElementById('turn-on')
-const $turnOff = document.getElementById('turn-off')
+const $colorOptions = document.getElementById('color-options')
 
 const $timeContent = document.getElementById('time-content')
+const $forSeconds = document.getElementById('for-seconds')
 const $fullDate = document.getElementById('full-date')
 const $dayOfWeek = document.getElementById('day-of-week')
 const $fullDateInfo = document.getElementById('full-date-info')
 const $dayOfWeekInfo = document.getElementById('day-week-info')
 
 const $settingsButton = document.getElementById('settings-button')
+const $settingsClose = document.getElementById('settings-close')
 const $additionalButton = document.getElementById('additional-button')
+const $infoClose = document.getElementById('info-close')
 
 // CODE 
 fetch('https://api.nasa.gov/planetary/apod?api_key=oLqSjn3VbbJDsQVqE8JXRpnjsh2dmaaFDXFjskHN&thumbs=True')
@@ -53,4 +52,108 @@ fetch('https://api.nasa.gov/planetary/apod?api_key=oLqSjn3VbbJDsQVqE8JXRpnjsh2dm
         }
     })
 
+$settingsButton.addEventListener('click', function() {
+    $settingsDisplay.setAttribute('style', 'display: inline-block')
+    $settingsButton.setAttribute('style', 'display: none')
+})
+$settingsClose.addEventListener('click', function() {
+    $settingsDisplay.setAttribute('style', 'display: none')
+    $settingsButton.setAttribute('style', 'display: inline-block')
+})
+
+$additionalButton.addEventListener('click', function() {
+    $additionalDisplay.setAttribute('style', 'display: inline-block')
+    $additionalButton.setAttribute('style', 'display: none')
+})
+$infoClose.addEventListener('click', function() {
+    $additionalDisplay.setAttribute('style', 'display: none')
+    $additionalButton.setAttribute('style', 'display: inline-block')
+})
+
+$colorOptions.addEventListener('change', function() {
+    localStorage.setItem('Selected Color', parseInt($colorOptions.value))
+    if(parseInt($colorOptions.value) == 1) {
+        $timeContent.setAttribute('style', 'color: white')
+        $forSeconds.setAttribute('style', 'color: white')
+    }
+    if(localStorage.getItem('Selected Color') == 2) {
+        $timeContent.setAttribute('style', 'color: green')
+        $forSeconds.setAttribute('style', 'color: green')
+    }
+    if(localStorage.getItem('Selected Color') == 3) {
+        $timeContent.setAttribute('style', 'color: yellow')
+        $forSeconds.setAttribute('style', 'color: yellow')
+    }
+})
+
+if(localStorage.getItem('Selected Color') == undefined) {
+    $timeContent.setAttribute('style', 'color: red')
+    $forSeconds.setAttribute('style', 'color: red')
+}
+if(localStorage.getItem('Selected Color') == 1) {
+    $timeContent.setAttribute('style', 'color: white')
+    $forSeconds.setAttribute('style', 'color: white')
+}
+if(localStorage.getItem('Selected Color') == 2) {
+    $timeContent.setAttribute('style', 'color: green')
+    $forSeconds.setAttribute('style', 'color: green')
+}
+if(localStorage.getItem('Selected Color') == 3) {
+    $timeContent.setAttribute('style', 'color: yellow')
+    $forSeconds.setAttribute('style', 'color: yellow')
+}
+
+
+
 // RUN 
+setInterval(function() {
+    const currentTime = new Date()
+    const hours = currentTime.getHours()
+    const minutes = currentTime.getMinutes()
+    const seconds = currentTime.getSeconds()
+    const miliseconds = currentTime.getMilliseconds()
+    const day = currentTime.getDay()
+
+    $timeContent.textContent = hours + ":" + minutes
+    $fullDateInfo.textContent = hours + ":" + minutes + ":" + seconds + ":" + miliseconds
+    if(day === 0) {
+        $dayOfWeekInfo.textContent = "Sunday"
+    }
+    if(day === 1) {
+        $dayOfWeekInfo.textContent = "Monday"
+    }
+    if(day === 2) {
+        $dayOfWeekInfo.textContent = "Tuesday"
+    }
+    if(day === 3) {
+        $dayOfWeekInfo.textContent = "Wednesday"
+    }
+    if(day === 4) {
+        $dayOfWeekInfo.textContent = "Thursday"
+    }
+    if(day === 5) {
+        $dayOfWeekInfo.textContent = "Friday"
+    }
+    if(day === 6) {
+        $dayOfWeekInfo.textContent = "Saturday"
+    }
+
+    $secondsOptions.addEventListener('change', function() {
+        localStorage.setItem('Seconds', parseInt($secondsOptions.value))
+        if(parseInt($secondsOptions.value) == 4) {
+            $forSeconds.textContent = seconds
+        }
+        if(parseInt($secondsOptions.value) == 5) {
+            $forSeconds.textContent = ''
+        }
+    })
+    if(localStorage.getItem('Seconds') == undefined) {
+        $forSeconds.textContent = ''
+    }
+    if(localStorage.getItem('Seconds') == 4) {
+        $forSeconds.textContent = seconds
+    }
+    if(localStorage.getItem('Seconds') == 5) {
+        $forSeconds.textContent = ''
+    }
+})
